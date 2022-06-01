26b6b915620fdc6988b6f0fb0f9caa335b1c429c
hashimoto31 <thairwars@gmail.com>
2022-06-01 21:38:12 +0900

    コンポーネント

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-function App() {
+// function App() {
+//   return (
+//     <>
+//       <p>Helloooooo</p>
+//       <input className="akiya" onClick={ () => { console.log(1122)} } />
+//     </>
+//   );
+// }
+
+const App = () => {
   return (
     <>
       <p>Helloooooo</p>
       <input className="akiya" onClick={ () => { console.log(1122)} } />
+      <Cat />
+      <Cat />
+      <Cat />
     </>
   );
+}
+
+const Cat = () => {
+  return (
+    <div>
+      にゃー
+    </div>
+  )
 }
 
 export default App;
